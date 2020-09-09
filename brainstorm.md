cad032060edde90ed822ef357f12d829de193579
conrad_buys <conradbuys11@gmail.com>
2020-09-09 09:14:39 -0400

    Added a bunch of jokes to seeds.rb, & added user stories to brainstorm.md.

--- brainstorm.md
+++ brainstorm.md
@@ -27,3 +27,8 @@ class JokeCategory
 	-Single Source of Truth (joke & category)
 end
 -------------------------------------
+As a user, I want to store my jokes so that I have a place to remember them when I need to impress people with my humor.
+As a user, I want to add my own jokes so that I can impress people with my humor.
+As a user, I want to search for types of jokes so that I can fit my humor to the crowd I'm with.
+As a user, I want to be able to rate jokes so that people don't have to bother with crappy humor.
+As a user, I want to be able to see which jokes I reviewed.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,49 @@
+User.create(
+    name: "Conrad"
+)
+
+User.create(
+    name: "Melinda"
+)
+
 Joke.create(
     title: "Chicken Coop Joke",
     setup: "Why does a chicken coop only have two doors?",
     punchline: "Because if it had four doors, it'd be a chicken sedan!"
-    )
+)
+
+Joke.create(
+    title: "Mermaid Joke",
+    setup: "Why do mermaids wear seashells as bras?",
+    punchline: "Because B-shells are too small!"
+)
+
+Joke.create(
+    title: "Shovel Joke",
+    setup: nil,
+    punchline: "The invention of the shovel was a groundbreaking discovery."
+)
+
+Joke.create(
+    title: "Duck Joke",
+    setup: "What's a duck's favorite drug?",
+    punchline: "Quack!"
+)
+
+Joke.create(
+    title: "Ghost Joke",
+    setup: "Why are ghosts banned from liquor stores?",
+    punchline: "Because they steal all the boos!"
+)
+
+Joke.create(
+    title: "Glasses Joke",
+    setup: "You should wear glasses when doing math.",
+    punchline: "It improves di-vision!"
+)
+
+Joke.create(
+    title: "Holy Water Joke",
+    setup: "How do you make holy water?",
+    punchline: "You boil the hell out of it!"
+)
